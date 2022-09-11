a2516566f71a0a6066b51d08af175603f7107c30
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2022-09-11 15:12:43 -0400

    Folder clean-up and new secret for tap-gui viewer

---
+++ post-install-scripts/install-store-ca-cert.sh
@@ -0,0 +1,50 @@
+VIEW_CLUSTER="tap-view"
+BUILD_CLUSTER="build-cluster"
+
+#Set context to view cluster
+kubectx $VIEW_CLUSTER
+
+#Retrieve Metadata Store CA Cert
+CA_CERT=$(kubectl get secret -n metadata-store ingress-cert -o json | jq -r ".data.\"ca.crt\"")
+
+#Retrieve Supply Chain Security Tools - Store’s Auth token
+AUTH_TOKEN=$(kubectl get secrets metadata-store-read-write-client -n metadata-store -o jsonpath="{.data.token}" | base64 -d)
+
+#Set context to Build cluster
+kubectx $BUILD_CLUSTER
+
+# Create secrets namespace
+kubectl create ns metadata-store-secrets
+
+# Create the CA Certificate secret
+cat <<EOF | kubectl create -f -
+apiVersion: v1
+kind: Secret
+type: Opaque
+metadata:
+  name: store-ca-cert
+  namespace: metadata-store-secrets
+data:
+  ca.crt: $CA_CERT
+EOF
+
+kubectl create secret generic store-auth-token --from-literal=auth_token="$AUTH_TOKEN" -n metadata-store-secrets
+
+cat <<EOF | kubectl apply -f -
+---
+apiVersion: secretgen.carvel.dev/v1alpha1
+kind: SecretExport
+metadata:
+  name: store-ca-cert
+  namespace: metadata-store-secrets
+spec:
+  toNamespaces: [tap-dev]
+---
+apiVersion: secretgen.carvel.dev/v1alpha1
+kind: SecretExport
+metadata:
+  name: store-auth-token
+  namespace: metadata-store-secrets
+spec:
+  toNamespaces: [tap-dev]
+EOF
