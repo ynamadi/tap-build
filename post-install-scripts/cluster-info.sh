25817f85171f9fe2139304f10ccd74863c7049c5
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2022-10-21 09:31:05 -0400

    Tekton Pipeline and Scan policies

--- post-install-scripts/cluster-info.sh
+++ post-install-scripts/cluster-info.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 CLUSTER_URL=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
-CLUSTER_TOKEN=$(kubectl -n tap-gui get secret $(kubectl -n tap-gui get sa tap-gui-viewer -o=json \
-| jq -r '.secrets[0].name') -o=json \
-| jq -r '.data["token"]' \
-| base64 --decode)
-# Print out each cluster's details for manual copy and paste
-echo $cluster "cluster URL:" $CLUSTER_URL
-echo $cluster "cluster token:" $CLUSTER_TOKEN
+
+CLUSTER_TOKEN=$(kubectl create token tap-gui-viewer --namespace tap-gui)
+
+echo CLUSTER_URL: $CLUSTER_URL
+echo CLUSTER_TOKEN: $CLUSTER_TOKEN
