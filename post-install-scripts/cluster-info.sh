3222c260479e332eb0cb5c5664f8987b8bdbfb64
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2022-10-31 10:27:44 -0400

    New Tekton Pipeline

--- post-install-scripts/cluster-info.sh
+++ post-install-scripts/cluster-info.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 CLUSTER_URL=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
 
-CLUSTER_TOKEN=$(kubectl create token tap-gui-viewer --namespace tap-gui)
+CLUSTER_TOKEN=$(kubectl create token tap-gui-viewer --namespace tap-gui --duration=488h)
 
 echo CLUSTER_URL: $CLUSTER_URL
 echo CLUSTER_TOKEN: $CLUSTER_TOKEN
