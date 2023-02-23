7061a063046a2f2e5c5ea7eae8366965c15e094c
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2023-02-23 12:00:04 -0500

    Adding CVE to the ignore list

---
+++ post-install-scripts/export-store-secrets.sh
@@ -0,0 +1,23 @@
+BUILD_CLUSTER="tap-build"
+
+#Set context to view cluster
+kubectx $BUILD_CLUSTER
+
+cat <<EOF | kubectl apply -f -
+---
+apiVersion: secretgen.carvel.dev/v1alpha1
+kind: SecretExport
+metadata:
+  name: store-ca-cert
+  namespace: metadata-store-secrets
+spec:
+  toNamespaces: [tap-dev]
+---
+apiVersion: secretgen.carvel.dev/v1alpha1
+kind: SecretExport
+metadata:
+  name: store-auth-token
+  namespace: metadata-store-secrets
+spec:
+  toNamespaces: [tap-dev]
+EOF
